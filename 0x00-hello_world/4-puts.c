a15502ae3bf7f2c94d82e205e3c4d9b81ebe5a7d
sofonias gashaw dubale <zephaniahsofa@gmail.com>
2023-06-16 01:29:38 -0700

    added puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Return is always 0(success)
+ *
+ */
+int main(void)
+{
+  char sentence[100]="Programming is like bulding a multilingual puzzle";
+    puts(sentence);
+
+  return (0);
+}
