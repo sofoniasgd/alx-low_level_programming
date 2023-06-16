9316dd0ae3dc079dc826610268f81be5c940c3bc
sofonias gashaw dubale <zephaniahsofa@gmail.com>
2023-06-16 05:37:04 -0700

    added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/*
+ *main - entry point
+ *this program prints a string using printf
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
