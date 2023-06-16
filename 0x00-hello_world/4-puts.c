a56011bf8b57e6c0d67c2b0094d75e80c5cefcbf
sofonias gashaw dubale <zephaniahsofa@gmail.com>
2023-06-16 03:26:40 -0700

    fixed 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,11 +6,9 @@
  * Return is always 0(success)
  *
  */
-int main()
+int main(void)
 {
-
 char sentence[100] = "\"Programming is like building a multilingual puzzle";
 puts(sentence);
 return (0);
-
 }
