6a8b225b8b329cb040608f612655b507d1bbe042
sofonias gashaw dubale <zephaniahsofa@gmail.com>
2023-06-19 19:48:47 -0700

    added print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int n;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+putchar(10);
+return (0);
+}
