4d4862fbc0229d905912a775e1ba35e3d7a42899
sofonias gashaw dubale <zephaniahsofa@gmail.com>
2023-06-20 14:44:58 -0700

    added abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - checks sign of argument
+ * @x: passed int value
+ * Return: absolute value of argument
+ */
+int _abs(int x)
+{
+if (x >= 0)
+{
+return (x);
+}
+else
+{
+return (-1 * x);
+}
+}
