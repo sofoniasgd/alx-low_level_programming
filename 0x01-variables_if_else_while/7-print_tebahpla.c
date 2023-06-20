a5f59be3cc1dc44d3209c61b1c3ab54611ee7a85
sofonias gashaw dubale <zephaniahsofa@gmail.com>
2023-06-19 19:52:00 -0700

    added print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar(10);
+return (0);
+}
