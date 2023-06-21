37c2a9042f82f2536bb5573040630e5d9f505803
sofonias gashaw dubale <zephaniahsofa@gmail.com>
2023-06-21 13:02:19 -0700

    added 8-24 hours

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -8,12 +8,6 @@
  */
 int main(void)
 {
-int r;
-
-print_last_digit(98);
-print_last_digit(0);
-r = print_last_digit(INT_MIN);
-_putchar('0' + r);
-_putchar('\n');
-return (0);
+    jack_bauer();
+    return (0);
 }
