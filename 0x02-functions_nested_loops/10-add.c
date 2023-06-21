9fd00a592214d94213fe017b428f1b8d846c23b6
sofonias gashaw dubale <zephaniahsofa@gmail.com>
2023-06-21 15:44:10 -0700

    added add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * add - adds two arguments
+ * @x,y: passed int value
+ * Return: sum of arguments
+ */
+int add(int x, int y)
+{
+return (x + y);
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -8,6 +8,9 @@
  */
 int main(void)
 {
-times_table();
-return (0);
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
 }
