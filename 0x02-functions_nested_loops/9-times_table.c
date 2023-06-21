9160454f92f0be1019a7684f850947b10d4449e6
sofonias gashaw dubale <zephaniahsofa@gmail.com>
2023-06-21 15:38:32 -0700

    fix 2 of (added times table)

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,54 +1,47 @@
 #include "main.h"
-
 /**
  * times_table - prints times table
- *
  * Return: none
  */
 void times_table(void)
 {
-int i, j, tmp1, tmp2, tmp3;
+int i, j;
 for (i = 0; i <= 9 ; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-tmp1 = (i *j);
-tmp2 = (tmp1 / 10) + 48;
-tmp3 = (tmp1 % 10) + 48;
 if (j != 9)
 {
-if (tmp1 >= 10)
+if (i * (j + 1) >= 10 && (i * j) < 10)
 {
-_putchar(8);
-_putchar(tmp2);
-_putchar(tmp3);
+_putchar((i * j) + 48);
+_putchar(44);
+_putchar(32);
+}
+else if (i * (j + 1) >= 10 && (i * j) >= 10)
+{
+_putchar(((i * j) / 10) + 48);
+_putchar(((i * j) % 10) + 48);
+_putchar(44);
+_putchar(32);
+}
+else
+{
+_putchar((i * j) + 48);
 _putchar(44);
 _putchar(32);
 _putchar(32);
+}}
+else
+{
+if ((i * j) >= 10)
+{
+_putchar(((i * j) / 10) + 48);
+_putchar(((i * j) % 10) + 48);
 }
 else
 {
-_putchar(tmp1 + 48);
-_putchar(44);
-_putchar(32);
-_putchar(32);
-}
-}
-else
-{
-if (tmp1 >= 10)
-{
-_putchar(8);
-_putchar(tmp2);
-_putchar(tmp3);
+_putchar((i * j) + 48);
+}}}
 _putchar(10);
-}
-else
-{
-_putchar(tmp1 + 48);
-_putchar(10);
-}
-}
-}
-}
-}
+}}
