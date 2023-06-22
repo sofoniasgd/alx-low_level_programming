47b41f06d4548a1ac9eaad93dec34a4fd0f646b9
sofonias gashaw dubale <zephaniahsofa@gmail.com>
2023-06-22 06:39:28 -0700

    added print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints integers 0-9
+ *
+ * Return: none
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+_putchar(i + 48);
+}
+_putchar(10);
+}
+
