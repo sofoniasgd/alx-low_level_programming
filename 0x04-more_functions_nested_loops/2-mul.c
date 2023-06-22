9922221811cc3fc9f0dcadb587176b2425aa5d07
sofonias gashaw dubale <zephaniahsofa@gmail.com>
2023-06-22 06:26:04 -0700

    added mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiples two integers
+ * @a: first int value
+ * @b: second int value
+ * Return: multiplied int
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
+
