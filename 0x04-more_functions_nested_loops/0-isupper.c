d85d3c1e3f45aa528509bc92ac5c4587827a3bc8
sofonias gashaw dubale <zephaniahsofa@gmail.com>
2023-06-22 05:37:12 -0700

    added isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks case of argument
+ * @c: passed int value
+ * Return: 1 if n is upper, 0 if lowercase
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+Alx functions and loops second project
+as if the first one wasnt stressing enough :)
