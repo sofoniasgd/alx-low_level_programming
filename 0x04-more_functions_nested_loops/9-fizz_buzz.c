e3e356d19583375dd5679b119910e3475e495c46
sofonias gashaw dubale <zephaniahsofa@gmail.com>
2023-06-22 18:48:21 -0700

    fixed(added fizz buzz)x2

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,11 +11,11 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
-printf("FIZZ ");
+printf("Fizz ");
 else if (i % 3 != 0 && i % 5 == 0)
-printf("BUZZ ");
+printf("Buzz ");
 else if (i % 3 == 0 && i % 5 == 0)
-printf("FIZZBUZZ ");
+printf("FizzBuzz ");
 else
 printf("%i ", i);
 }
