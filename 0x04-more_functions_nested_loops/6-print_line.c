e29ca05d4e951622a10af0cd72f5d7a462833a62
sofonias gashaw dubale <zephaniahsofa@gmail.com>
2023-06-22 17:57:28 -0700

    added print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straignht line
+ * @n: length of line
+ * Return: none
+ */
+void print_line(int n)
+{
+int i;
+if (n <= 0)
+{
+
+}
+else
+{
+for (i = 0; i < n; i++)
+{
+_putchar(95);
+}
+}
+_putchar(10);
+}
+
