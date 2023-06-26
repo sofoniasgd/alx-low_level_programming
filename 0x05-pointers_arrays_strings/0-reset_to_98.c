b2740f364bfb732985754f3984108179fe03b008
sofonias gashaw dubale <zephaniahsofa@gmail.com>
2023-06-26 08:14:51 -0700

    first commit for 0x05 folder

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes variable pointed to 98
+ * @*n: passed pointer
+ *
+ * Return: none
+ *
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is the readme for pointers, arraya and strings projects folder, it has 10 main + 2 advanced tasks
