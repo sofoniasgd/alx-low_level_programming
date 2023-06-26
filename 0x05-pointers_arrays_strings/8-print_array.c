f61588570c33faa1d66cd48d2886faa3101891d3
sofonias gashaw dubale <zephaniahsofa@gmail.com>
2023-06-26 13:54:46 -0700

    added print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,12 +11,19 @@
 void print_array(int *a, int n)
 {
 	int i;
-	for (i = 0; i < n; i++)
+	if (n > 0 )
 	{
-		printf("%d", *(a + i));
-		if (i == (n - 1))
-			printf("\n");
-		else
-			printf(", ");
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", *(a + i));
+			if (i == (n - 1))
+				printf("\n");
+			else
+				printf(", ");
+		}
+	}
+	else
+	{
+	printf("\n");
 	}
 }
