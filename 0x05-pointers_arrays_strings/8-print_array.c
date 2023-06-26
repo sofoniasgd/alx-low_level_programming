ab641b7b6de2f89ef13ae91bb17835b4fb2e5d47
sofonias gashaw dubale <zephaniahsofa@gmail.com>
2023-06-26 13:49:32 -0700

    added print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints n elements of array
+ * @a: pointer variable
+ * @n: number of elements to print
+ * Return: none
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i == (n - 1))
+			printf("\n");
+		else
+			printf(", ");
+	}
+}
