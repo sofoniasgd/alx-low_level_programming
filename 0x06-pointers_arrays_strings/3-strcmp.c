70a793e0dd953a4fdded320e251d9bdcb106944e
sofonias gashaw dubale <zephaniahsofa@gmail.com>
2023-06-29 14:03:27 -0700

    added strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * _strcmp - compares one string to another
+ * @s1: 1st string
+ * @s2: 2nd string
+ *
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int lens1, lens2, loop, i, ret;
+ret = 0;
+lens1 = length(s1);
+/* printf("length of dest - %d\n", lend); */
+lens2 = length(s2);
+/* printf("length of src - %d\n", lens);*/
+
+if (lens1 >= lens2)
+	{
+	loop = lens1;
+	}
+else
+	{
+	loop = lens2;
+	}
+for (i = 0; i < loop; i++)
+	{
+	ret = ret + (*(s1 + i) - *(s2 + i));
+	}
+return (ret);
+}
+
+/**
+ * length - finds length of a string
+ * @s: pointer to string
+ * Return: number of length
+ */
+
+int length(char *s)
+{
+int len, i;
+len = 0;
+i = 0;
+while (*(s + i) != '\0')
+{
+len++;
+i++;
+}
+return (len);
+}
