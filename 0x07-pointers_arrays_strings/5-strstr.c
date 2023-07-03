b3a0a977d4e2da41cd2d793bbb032c098459bd05
sofoniasgd <zephaniahsofa@gmail.com>
2023-07-03 18:02:02 +0000

    added strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include<stddef.h>
+/**
+ * _strstr - locates an  entire substring from haystack
+ * @haystack: source string
+ * @needle: substring
+ * Return: starting location of substring or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i;
+while (*haystack != '\0')
+	{
+	if (*haystack == *needle)
+		{
+		i = 0;
+		while (*(needle + i) != '\0')
+			{
+			if (*(haystack + i) == *(needle + i))
+				{
+				}
+			else
+				{
+				break;
+				}
+			i++;
+			}
+		if (*(needle + i) == '\0')
+			{
+			return (haystack);
+			}
+		}
+	haystack++;
+	}
+return (NULL);
+}
