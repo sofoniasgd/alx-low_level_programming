799a76dc4fe409a4c3c40aea5e877758d1a00ec8
sofoniasgd <zephaniahsofa@gmail.com>
2023-07-03 15:44:50 +0000

    added strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include<stddef.h>
+/**
+ * _strspn - a function that returns a length of matching chars
+ * @s: source location pointer
+ * @accept: character list to be searched
+ * Return: length of occured chars or NULL
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+int i, j, match, flag, len;
+i = 0;
+match = 0;
+flag = 0;
+len = 0;
+while (*(s + i) != '\0')
+	{
+	j = 0;
+	while (*(accept + j) != '\0')
+		{
+		if (*(accept + j) == *(s + i))
+			{
+			match = 1;
+			}
+		j++;
+		}
+	if (match == 0 && flag == 0)
+		{
+		continue;
+		}
+	else if (match == 1 && flag == 0)
+		{
+		flag = 1;
+		len++;
+		}
+	else if (match == 1 && flag == 1)
+		{
+		len++;
+		}
+	else if (match == 0 && flag == 1)
+		{
+		break;
+		}
+	match = 0;
+	i++;
+	}
+return (len);
+}
