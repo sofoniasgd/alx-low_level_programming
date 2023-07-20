978c39df7ce0449306994176bc3182d8fdd2ae76
sofoniasgd <zephaniahsofa@gmail.com>
2023-07-20 09:40:45 +0000

    added 101 print comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * main - prints three digit numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int i, j, k;
+for (i = 0; i < 10; i++)
+{
+	for (j = 0; j < 10; j++)
+	{
+		for (k = 0; k < 10; k++)
+		{
+			if (k > j && j > i)
+			{
+				putchar(i + 48);
+				putchar(j + 48);
+				putchar(k + 48);
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+}
+putchar(10);
+return (0);
+}
