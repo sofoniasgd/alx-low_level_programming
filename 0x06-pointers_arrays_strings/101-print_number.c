e8d1210eea9e8f8975821eb1b968c0be43dcb2a5
sofoniasgd <zephaniahsofa@gmail.com>
2023-08-02 09:03:45 +0000

    added 101 print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,11 +1,32 @@
 #include "main.h"
 
 /**
+ * power - calculate power of ten
+ * @c: exponent
+ * Return: 10, 100, 1000,1 multiple of 10 power x
+ */
+int power(int c)
+{
+int ans, i;
+ans = 1;
+if (c == 0)
+{
+return (1);
+}
+else
+{
+for (i = 0; i < c; i++)
+{
+ans = ans * 10;
+}
+return (ans);
+}
+}
+/**
  * print_number - prints an int using putchar
  * @n: passed int value
- * Return: 1 if n is upper, 0 if lowercase
+ * Return: none
  */
-int power(int c);
 void print_number(int n)
 {
 int s, num, len, i, d;
@@ -38,27 +59,5 @@ for (i = len; i >= 1; i--)
 {
 s = (num % power(i)) / power(i - 1);
 _putchar(s + 48);
-}
-}
-/**
- * power - calculate power of ten
- * @c: exponent
- * Return: 10, 100, 1000,1 multiple of 10 power x
- */
-int power(int c)
-{
-int ans, i;
-ans = 1;
-if (c == 0)
-{
-return (1);
-}
-else
-{
-for (i = 0; i < c; i++)
-{
-ans = ans * 10;
-}
-return (ans);
 }
 }
