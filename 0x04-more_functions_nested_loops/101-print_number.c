88939063a2d4902cd1cf755bf7032dc404603ec3
sofoniasgd <zephaniahsofa@gmail.com>
2023-08-02 14:43:47 +0000

    fixed(added print number)x3

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,46 +1,6 @@
 #include "main.h"
 
 /**
- * print_number - prints an int using putchar
- * @n: passed int value
- * Return: 1 if n is upper, 0 if lowercase
- */
-
-void print_number(int n)
-{
-int s, num, len, i, d;
-len = 0;
-num = n;
-d = 1;
-if (num < 0)
-{
-num = num * -1;
-_putchar(45);
-}
-if (n == 0)
-{
-_putchar(48);
-}
-for (i = 0; i < 18; i++)
-{
-
-if (num >= d)
-{
-len++;
-}
-else
-{
-continue;
-}
-d = d * 10;
-}
-for (i = len; i >= 1; i--)
-{
-s = (num % power(i)) / power(i - 1);
-_putchar(s + 48);
-}
-}
-/**
  * power - calculate power of ten
  * @c: exponent
  * Return: 10, 100, 1000,1 multiple of 10 power x
@@ -62,3 +22,51 @@ ans = ans * 10;
 return (ans);
 }
 }
+/**
+ * print_number - prints an int using putchar
+ * @n: passed int value
+ * Return: none
+ */
+void print_number(int n)
+{
+int s, len, i;
+unsigned int num, d;
+len = 0;
+d = 1;
+num = 0;
+/* if n is -ve, flip it and print - */
+if (n < 0)
+{
+num = (unsigned int)(n * -1);
+_putchar(45);
+}
+else
+{
+num = (unsigned int)n;
+}
+if (n == 0)
+{
+_putchar(48);
+}
+/* finds the length if the int n */
+for (i = 0; i < 10; i++)
+{
+d = (i == 0) ? 1 : (d * 10);
+if (num >= d)
+{
+len++;
+continue;
+}
+else
+{
+break;
+}}
+/* prints n */
+for (i = len; i >= 1; i--)
+{
+	if (i == 10)
+		s = num / power(i - 1);
+	else
+		s = (num % power(i)) / power(i - 1);
+_putchar(s + 48);
+}}
