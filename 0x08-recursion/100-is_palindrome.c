64dec253ae9e20b2aa339e46d1a3e9e46f605c87
sofoniasgd <zephaniahsofa@gmail.com>
2023-08-03 17:40:38 +0000

    final added=> 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "main.h"
+/* #include<stdio.h> */
 /**
  * chkpalindrome - function to traverse a set of matching chars
  * @s1: pointer to first of two matching chars
@@ -10,11 +11,13 @@ int chkpalindrome(char *s1, char *s2)
 {
 if (*s2 == '\0')
 	{
+	/* printf(" end p "); */
 	return (1);
 	}
 if (*s1 == *s2)
 	{
-	return (1 * chkpalindrome((s1 - 1), (s2 + 1)));
+	/* printf(" p "); */
+	return (1 + chkpalindrome((s1 - 1), (s2 + 1)));
 	}
 	return (0);
 }
@@ -27,15 +30,22 @@ if (*s1 == *s2)
 
 int is_palindrome(char *s)
 {
+	int tmp;
 	/* checks for empty string*/
 if (*s == '\0')
 	{
+	/* printf(" s = '\\0' "); */
 	return (1);
 	}
 /* checks for a four letter palinfrome => edde, reer */
 else if ((*s == *(s + 1)) && (*(s - 1) == *(s + 2)))
 	{
-	return (chkpalindrome(s, (s + 1)));
+	/* printf(" xyyx "); */
+	tmp = chkpalindrome(s, (s + 1));
+	if (*(s + tmp) == '\0')
+		{
+		return (1);
+		}
 	}
 /*
  * checks for a four letter palindrome separated by another letter
@@ -43,14 +53,21 @@ else if ((*s == *(s + 1)) && (*(s - 1) == *(s + 2)))
  */
 else if ((*(s - 1) == *(s + 1)) && (*(s - 2) == *(s + 2)))
 	{
-	return (chkpalindrome((s - 1), (s + 1)));
+	/* printf(" xyzyx "); */
+	tmp = chkpalindrome((s - 1), (s + 1));
+	if (*(s + tmp) == '\0')
+		{
+		return (1);
+		}
 	}
 /* checks for end of string meaning no palindrome */
 if (*(s + 1) == '\0')
 	{
+	/* printf(" no p "); */
 	return (0);
 	}
 /* continue recursion */
+
 	return (1 * is_palindrome((s + 1)));
 
 }
