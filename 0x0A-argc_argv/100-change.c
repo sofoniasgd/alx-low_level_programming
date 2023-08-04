938b4f37d55a06c3291b31e009562dc9cd5d0e5f
sofoniasgd <zephaniahsofa@gmail.com>
2023-08-04 21:35:11 +0000

    added 100 change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - prints product of two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0 (success)
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+int coins, amount;
+double money, sum;
+/* runs only if there is an argument supplied(amount of coins) */
+if (argc == 2)
+	{
+	coins = atoi(argv[1]);
+	/* print zero if argument is negative*/
+	if (coins < 0)
+		{
+		printf("%i\n", 0);
+		}
+
+	amount = (double)coins * 0.1;
+	if (floor(amount) == amount)
+		{
+
+		}
+	}
+/* else print error*/
+else
+	{
+	printf("Error\n");
+	return (1);
+	}
+return (0);
+}
