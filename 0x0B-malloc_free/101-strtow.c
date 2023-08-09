ba11a839ac6b525182f09ab150acd9a85386ec8c
sofoniasgd <zephaniahsofa@gmail.com>
2023-08-09 22:13:18 +0000

    added 101 strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,13 +3,49 @@
 #include<stdlib.h>
 
 /**
- * strtow - returns a pointer to a 2D array of int
+ * strtow - function that splits a string into words
  * @str: pointer to start of string
- * Return: pointer to the split strings
+ * Return: pointer to the split strings double pointer
  */
-int **strtow(char *str)
+char **strtow(char *str)
 {
-char **test;
+char **split;
+int i, j, k, row;
+row = 0;
 
-return (test);
+if (str == NULL || *str == '\0')
+{return (NULL); }
+/* loop to find start of strings=> row */
+/* nested loop to find size of words=> column */
+i = 0;
+while (*(str + i) != '\0')
+	{
+	if (*(str + i) != ' ' && *(str + (i - 1)) == ' ')
+		{row++; }
+	i++;
+	}
+split = (char **)malloc(sizeof(char) * (row + 1));
+if (split == NULL)
+{return (NULL); }
+i = 0;
+row = 0;
+while (*(str + i) != '\0')
+	{
+	if (*(str + i) != ' ' && *(str + (i - 1)) == ' ')
+		{
+		j = 0;
+		while (*(str + i + j) != ' ')
+			{j++; }
+		split[row] = (char *)malloc(sizeof(char) * (j + 2));
+		if (split[row] == NULL)
+		{return (NULL); }
+		for (k = 0; k < (j); k++)
+			{split[row][k] = *(str + i + k); }
+		split[row][j] = '\0';
+		row++;
+		}
+	i++;
+	}
+split[row] = NULL;
+return (split);
 }
