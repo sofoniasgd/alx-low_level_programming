7a9d27e7ce30661c8a571d2be210dd33a9b74342
sofoniasgd <zephaniahsofa@gmail.com>
2023-08-09 18:06:24 +0000

    fix1(100 argstostr)

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,14 +3,70 @@
 #include<stdlib.h>
 
 /**
- * argstotr - returns a pointer to a 2D array of int
- * @ac: number of arguments
- * @avL pointer to arguments
- * Return: pointer to the new strings
+ * findlen - finds length of a string
+ * @s: pointer to string
+ * Return: length of the string
  */
-int *argstotr(int ac, char **av)
-{
-char *test;
+int findlen(char *s)
+	{
+	int i;
+	char *s1;
 
-return (test);
+	s1 = s;
+	i = 0;
+	while (*(s1 + i) != '\0')
+		{
+		i++;
+		}
+	return (i);
+	}
+/**
+ * argstostr - concatenates all arguments
+ * @ac: number of arguments
+ * @av: pointer to arguments
+ * Return: pointer to the concatenated string
+ */
+char *argstostr(int ac, char **av)
+{
+char *ret;
+int i, j, totalchars, inew, column;
+
+totalchars = 0;
+/* checking for NULL values in ac & av */
+if (ac == 0 || av == NULL)
+	{
+	return (NULL);
+	}
+/* finding and adding number of chars in each argument */
+/* to get total number of chars including new line */
+for (i = 1; i < ac; i++)
+	{
+	totalchars += (findlen(av[i]) + 1);
+	}
+/* allocating the memory and catching errors */
+ret = malloc(sizeof(char) * totalchars);
+if (ret == NULL)
+{return (NULL); }
+inew = 0;
+for (i = 1; i < ac; i++)
+	{
+	column = findlen(av[i]) + 1;
+	for (j = 0; j < (column - 1); j++)
+		{
+		*(ret + inew) = av[i][j];
+		inew++;
+		}
+	if (i == (ac - 1))
+		{
+		*(ret + inew) = '\n';
+		inew++;
+		*(ret + inew) = '\0';	
+		}
+	else
+		{
+		*(ret + inew) = '\n';
+		inew++;
+		}
+	}
+return (ret);
 }
