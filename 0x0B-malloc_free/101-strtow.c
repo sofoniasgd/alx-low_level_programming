a3ccea411495a384de49c924a951cdef6b517218
sofoniasgd <zephaniahsofa@gmail.com>
2023-08-10 00:15:25 +0000

    minor fix strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -16,36 +16,51 @@ row = 0;
 if (str == NULL || *str == '\0')
 {return (NULL); }
 /* loop to find start of strings=> row */
-/* nested loop to find size of words=> column */
 i = 0;
 while (*(str + i) != '\0')
 	{
-	if (*(str + i) != ' ' && *(str + (i - 1)) == ' ')
+	if (*(str + i) != ' ' && (*(str + (i - 1)) == ' ' || i == 0))
 		{row++; }
 	i++;
 	}
 split = (char **)malloc(sizeof(char) * (row + 1));
 if (split == NULL)
-{return (NULL); }
+	{
+	free(split);
+	return (NULL);
+	}
 i = 0;
 row = 0;
 while (*(str + i) != '\0')
 	{
-	if (*(str + i) != ' ' && *(str + (i - 1)) == ' ')
+	if (*(str + i) != ' ' && (i == 0 || *(str + (i - 1)) == ' '))
 		{
 		j = 0;
 		while (*(str + i + j) != ' ')
-			{j++; }
-		split[row] = (char *)malloc(sizeof(char) * (j + 2));
+			{
+			if (*(str + i + j) == '\0')
+			{break; }
+			j++;
+			}
+		split[row] = malloc(sizeof(char) * (j + 1));
 		if (split[row] == NULL)
-		{return (NULL); }
-		for (k = 0; k < (j); k++)
-			{split[row][k] = *(str + i + k); }
-		split[row][j] = '\0';
-		row++;
+			{
+			free(split[row]);
+			return (NULL);
+			}
+		for (k = 0; k <= j; ++k)
+			{
+			if (k == j)
+				split[row][k] = '\0';
+			else
+				split[row][k] = *(str + i + k);
+			}
+			/* printf("split[%i][%i] - %c\n", row, j, split[row][0]); */
+		++row;
 		}
-	i++;
+	++i;
 	}
+/* printf("split[0][0] - %c\n", split[0][0]); */
 split[row] = NULL;
 return (split);
 }
