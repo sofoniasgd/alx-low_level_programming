5556c53a3aa8d9981a26036bface3e25018d2e0c
sofoniasgd <zephaniahsofa@gmail.com>
2023-08-11 12:48:20 +0000

    added 100 realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,25 +2,52 @@
 #include<stdio.h>
 #include<stddef.h>
 #include<stdlib.h>
+#include<string.h>
 
 /**
- * _realloc - allocates memory block
- * @ptr: pointer
- * @old_size: old size
- * @new_size: new size
- * Return: pointer to allocated mempry
+ * _realloc - reallocates memory block from src to dest
+ * @ptr: pointer to source memory block
+ * @old_size: size of source memory block in bytes
+ * @new_size: size of destination memory block in bytes
+ * Return: pointer to reallocated memory block
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-void *str;
-int i, j;
-i = old_size;
-j = new_size;
-i += j;
+void *newptr;
+int lengthcpy;
+
+if (new_size == old_size)
+	{
+	return (ptr);
+	}
+if (ptr == NULL)
+	{
+	newptr = malloc(new_size);
+	if (newptr == NULL)
+		{
+		return (NULL);
+		}
+	return (newptr);
+	}
+/* in this case the call is equivalent to free(ptr) */
 if (new_size == 0 && ptr != NULL)
-{
-return (NULL);
-}
-return (str);
+	{
+	free(ptr);
+	return (NULL);
+	}
+/* copy length => greater of source or destination*/
+if (new_size > old_size)
+	lengthcpy = old_size;
+else if (new_size < old_size)
+	lengthcpy = new_size;
+newptr = malloc(new_size);
+if (newptr == NULL)
+	{
+	return (NULL);
+	}
+/* hope i can use memcpy for the reallocation */
+memcpy(newptr, ptr, lengthcpy);
+free(ptr);
+return (newptr);
 }
