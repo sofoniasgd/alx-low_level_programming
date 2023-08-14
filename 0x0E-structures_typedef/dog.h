3ab119b9999757d1b381a2c4838c30821cf35e6a
sofoniasgd <zephaniahsofa@gmail.com>
2023-08-14 20:18:55 +0000

    added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define a new structure dog
+ * @name: name pointer
+ * @age: float for age
+ * @owner: pointer to string
+ */
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
