52c9377cb2287521516cce9a7701cbd2f5aba07d
sofoniasgd <zephaniahsofa@gmail.com>
2023-08-14 22:56:06 +0000

    added print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - print struct dog
+ * @d: pointer to structure argument
+ * Return: none
+ */
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+	{
+	exit(0);
+	}
+/* check name type */
+if (d->name == NULL)
+	printf("Name: (nil)\n");
+else
+	printf("Name: %s\n", d->name);
+/* check age type */
+	printf("Age: %f\n", d->age);
+/* check owner type */
+if (d->owner == NULL)
+	printf("(nil)\n");
+else
+	printf("Owner: %s\n", d->owner);
+}
