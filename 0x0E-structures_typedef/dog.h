12e353b6e71b2c3e344f217e4d04b693054793d1
sofoniasgd <zephaniahsofa@gmail.com>
2023-08-15 08:42:50 +0000

    updatedx2 dog.h for task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,13 +7,12 @@
  * @age: float for age
  * @owner: pointer to string
  */
-typedef struct dog dog_t;
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
