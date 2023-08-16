b0774df800dc7171c129c1d251b7558fe1177b4e
sofoniasgd <zephaniahsofa@gmail.com>
2023-08-16 20:58:43 +0000

    fix1(added 2-int_index.c)

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,8 +16,7 @@ int i;
 /* checking for NULL pointers */
 if (cmp == NULL || array == NULL)
 	{
-	exit(0);
-	return (0);
+	return (-1);
 	}
 /* if size is less than 0 return -1 */
 if (size <= 0)
@@ -39,6 +38,5 @@ for (i = 0; i < size; i++)
 		return (-1);
 		}
 	}
-exit(0);
 return (0);
 }
