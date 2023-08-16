ab39fc150d2d57580061896b031b1510c0c3ac60
sofoniasgd <zephaniahsofa@gmail.com>
2023-08-16 19:37:50 +0000

    fix1(added 0-print_name.c)

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include<stddef.h>
 /**
  * print_name - prints a name
  * @name: pointer to printable text
@@ -9,5 +9,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+if(f == NULL)
+{return; }
 f(name);
 }
