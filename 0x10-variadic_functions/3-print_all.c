069614b0acd4a9efb607531662c1c38df7ae943d
sofoniasgd <zephaniahsofa@gmail.com>
2023-08-18 04:14:17 +0000

    added skeletones

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,14 @@
+#include "variafic)functions.h"
+#include<stdio.h>
+#include<stddef.h>
+
+/**
+ * print_all - prints anything
+ * @format:
+ * 
+ */
+
+void print_all(const char * const format, ...)
+{
+
+}
