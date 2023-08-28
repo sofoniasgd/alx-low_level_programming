0510e48c98b9c80f6c45754fe6076f0a3de7db49
sofoniasgd <zephaniahsofa@gmail.com>
2023-08-28 13:30:44 +0000

    added task0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<stddef.h>
+/**
+ * print_listint - prints elements of a linked list
+ * @h: start of linked list
+ * Return: number of list elements
+ */
+size_t print_listint(const listint_t *h)
+{
+int count;
+const listint_t *current;
+count = 0;
+/* check if h is NULL */
+if (h == NULL)
+	return (count);
+current = malloc(sizeof(listint_t));
+current = h;
+/* traverse the linked list */
+while (current != NULL)
+	{
+	printf("%i\n", current->n);
+	count++;
+	current = current->next;
+	}
+return (count);
+}
