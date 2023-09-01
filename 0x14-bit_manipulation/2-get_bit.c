d377c497da16ac61378b2d8afd5a47c37caadaa9
sofoniasgd <zephaniahsofa@gmail.com>
2023-09-01 09:23:22 +0000

    fix1 task2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -16,7 +16,7 @@ result = 1;
 /* return 1 when n=0 -> 2^0=1 */
 if (n == 0)
 	return (result);
-if (n == 63)
+if (n == 64)
 	return (ULONG_MAX);
 for (i = 1; i <= n; i++)
 	{
@@ -33,30 +33,34 @@ return (result);
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-int i, j, ret;
+int i, j, ret, bit;
 unsigned long int twon, powerr;
-for (i = 63; i >= 0; i--)
+for (i = 64; i >= 0; i--)
 	{
 	twon = two_n(i);
-	if (n >= twon)
+	if (n >= twon || n == ULONG_MAX)
 		break;
 	}
-i++;
 ret = 0;
+bit = 0;
 j = i;
 if (index > (unsigned int)i)
-	return (-1);
+	return (0);
 for (j = i; j >= 0; j--)
 	{
 	powerr = two_n(j);
-	if (n >= powerr)
+	if (n == ULONG_MAX)
 		{
-		ret = 1;
+		bit = 1;
+		n = 0; }
+	else if (n >= powerr)
+		{
+		bit = 1;
 		n -= powerr; }
 	else
-		ret = 0;
+		bit = 0;
 	if (index == (unsigned int)j)
-		return (ret);
+		ret = bit;
 	}
-return (-1);
+return (ret);
 }
--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "main.h"
-
+#include<limits.h>
 /**
  * main - check the code
  *
@@ -11,10 +11,16 @@ int main(void)
     int n;
 
     n = get_bit(1024, 10);
-    printf("%d\n", n);
+    printf("\n%d\n", n);
     n = get_bit(98, 1);
-    printf("%d\n", n);
+    printf("\n%d\n", n);
     n = get_bit(1024, 0);
-    printf("%d\n", n);
+    printf("\n%d\n", n);
+    n = get_bit(2048, 20);
+    printf("\n%d\n", n);
+    n = get_bit(ULONG_MAX, 19);
+    printf("\n%d\n", n);
+    n = get_bit(ULONG_MAX, 0);
+    printf("\n%d\n", n);
     return (0);
 }
