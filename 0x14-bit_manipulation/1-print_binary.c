ed9c4c47af30f5ed66a1d2d5aae1365e75ad1c10
sofoniasgd <zephaniahsofa@gmail.com>
2023-09-01 13:23:50 +0000

    added task1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include<stdio.h>
+#include<stddef.h>
+#include<limits.h>
+#include<stdlib.h>
+/**
+ * two_n - calculates 2 raised to n
+ * @n: exponent of 2
+ * Return: result of 2^n
+ */
+unsigned long int two_n(int n)
+{
+int i;
+unsigned long int result;
+result = 1;
+/* return 1 when n=0 -> 2^0=1 */
+if (n == 0)
+	return (1);
+if (n == 64)
+	return (ULONG_MAX);
+for (i = 1; i <= n; i++)
+	{
+	result *= 2;
+	}
+return (result);
+}
+
+/**
+ * print_binary - prints binary representation of n
+ * @n: number
+ * Return: none
+ */
+void print_binary(unsigned long int n)
+{
+int i, j, istart;
+unsigned long int powerr;
+istart = 0;
+
+/* */
+for (i = 63; i >= 0; i--)
+	{
+	powerr = two_n(i);
+	if (n >= powerr)
+		{
+		istart = i;
+		break; }
+	}
+for (j = istart; j >= 0; j--)
+	{
+	powerr = two_n(j);
+	if (n >= powerr)
+		{
+		n -= powerr;
+		printf("%i", 1);
+		}
+	else
+		printf("%i", 0);
+	}
+}
