495ad12ae9e3f250b1902d86637a8a6c593d3d1c
sofoniasgd <zephaniahsofa@gmail.com>
2023-09-05 01:24:16 +0000

    added task3&4

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * main - custom cp implementation
+ *
+ * Return: Always 0
+ */
+int main(int ac, char **av)
+{
+/* wrong argument count */
+if (ac != 2)
+	{
+	fprintf(stderr, "Usage: cp file_from file_to\n");
+	exit (97); }
+
+
+}
