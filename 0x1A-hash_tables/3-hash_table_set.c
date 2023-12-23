9ddf19cdd03d79b37cdc2564028b6f6127efb1f2
sofoniasgd <zephaniahsofa@gmail.com>
2023-12-23 14:23:34 +0000

    updated task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,7 +10,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index, size;
-	hash_node_t *node;
+	hash_node_t *node, *tmp;
 
 	/*check if key or value exists*/
 	if (!key || !value || !ht)
@@ -24,19 +24,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
-	node->key = strdup((const char *)key);
-	node->value = strdup((const char *)value);
+	node->key = strdup(key);
+	node->value = strdup(value);
 	/*find index and check if its empty or not*/
-	/*if ((ht->array)[index] == NULL)*/
-	/*{*/
-	(ht->array)[index] = node;
-	node->next = NULL;
-	/*}*/
-	/*else*/
-	/*{*/
-	/*	tmp = (ht->array)[index];*/
-	/*	(ht->array)[index] = node;*/
-	/*	node->next = tmp;*/
-	/*}*/
+	if ((ht->array)[index] == NULL) 
+	{
+		(ht->array)[index] = node;
+		node->next = NULL;
+	}
+	else
+	{	
+		tmp = (ht->array)[index];
+		if (strcmp(tmp->key, key) == 0)
+		{
+			tmp->value = node->value;
+			return (1);
+		}
+		(ht->array)[index] = node;
+		node->next = tmp;
+	}
 	return (1);
 }
