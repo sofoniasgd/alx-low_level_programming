cd574719da74ca8387b7c91033de2f8b785b3516
sofoniasgd <zephaniahsofa@gmail.com>
2023-12-23 01:03:32 +0000

    updated task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,7 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->key = (char *)key;
 	node->value = (char *)value;
 	/*find index and check if its empty or not*/
-	if (!(ht->array)[index])
+	if ((ht->array)[index] == NULL)
 	{
 		(ht->array)[index] = node;
 		node->next = NULL;
