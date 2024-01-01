7a8df376a49a4fb1475a2837da4f69b41cd52970
sofoniasgd <zephaniahsofa@gmail.com>
2024-01-01 08:05:55 +0000

    changing working pcs

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
 /**
  * main - check the code
  *
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-    char s1[] = "Hello";
-    char s2[] = "World!";
+    char s1[] = "eret";
+    char s2[] = "PATH=NOWHEREimportant";
 
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
+    printf("s1 = s2-> %d\n", strcmp(s1, s2));
+    printf("s2 = s1-> %d\n", strcmp(s2, s1));
+    printf("s1 = s1-> %d\n", strcmp(s1, s1));
     return (0);
 }
