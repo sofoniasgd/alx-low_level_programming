80dab7c1e7334ea3d8c0813db8b8e033d1d4becb
sofoniasgd <zephaniahsofa@gmail.com>
2024-02-03 03:07:45 +0000

    updated 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,7 +7,7 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int i;
+	unsigned long int i, count = 0;
 	hash_node_t *node;
 
 	if (ht == NULL)
@@ -17,12 +17,17 @@ void hash_table_print(const hash_table_t *ht)
 	{
 		if ((ht->array)[i])
 		{
+			count++;
 			node = (ht->array)[i];
 			while (node->next != NULL)
 			{
-				printf("'%s': '%s', ", node->key, node->value);
+				if (count > 1)
+					printf(", ");
+				printf("'%s': '%s'", node->key, node->value);
 				node = node->next;
 			}
+			if (count > 1)
+				printf(", ");
 			printf("'%s': '%s'", node->key, node->value);
 		}
 	}
