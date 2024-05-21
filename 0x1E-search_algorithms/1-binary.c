c2f1c48e99d4dac97033b2a783e69d19f984dc8d
root <root@DESKTOP-NA9HGAG>
2024-05-22 02:16:25 +0300

    task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,6 +2,25 @@
 #include <math.h>
 
 /**
+ * print_array - prints arrays using start and end pointer
+ * @array: pointer to array
+ * @start: start index
+ * @end: end index
+ * Return: none
+ */
+void print_array(int *array, size_t start, size_t end)
+{
+	size_t i;
+
+	i = start;
+	while (i < end)
+	{
+		printf("%i,", array[i]);
+		i++;
+	}
+	printf("%i\n", array[end]);
+}
+/**
  * binary_search - searches for a value using the Binary search algorithm
  * @array: pointer to sorted int array
  * @size: ssize of array
@@ -11,27 +30,23 @@
 int binary_search(int *array, size_t size, int value)
 {
 	/* set left and right markers */
-	size_t L = 0, R = (size - 1), diff = 0, i;
+	size_t L = 0, R = (size - 1), diff = 0;
 
 	if (!array)
 		return (-1);
 	printf("Searching in array: ");
-	for (i = L; i < R; i++)
-		printf("%i,", array[i]);
-	printf("%li\n", R);
-	while (L <= R)
+	print_array(array, L, R);
+	while (L < R)
 	{
 		diff = (L + R) / 2;
 		if (array[diff] < value)
-			L += (diff + 1);
+			L = (diff + 1);
 		else if (array[diff] > value)
-			R -= (diff - 1);
+			R = (diff - 1);
 		else
-			return (diff + L);
+			return (diff);
 		printf("Searching in array: ");
-		for (i = L; i < R; i++)
-			printf("%i,", array[i]);
-		printf("%li\n", R);
+		print_array(array, L, R);
 	}
 	return (-1);
 }
