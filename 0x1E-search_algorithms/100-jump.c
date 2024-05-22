a20b2d0443efef6ada13f16a7a200307a3c4997e
sofoniasgd <zephaniahsofa@gmail.com>
2024-05-22 14:31:29 +0300

    updated task 7

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -33,7 +33,7 @@ int jump_search(int *array, size_t size, int value)
 			break;
 		printf("Value checked array[%li] = [%i]\n", j, array[j]);
 		if (array[j] == value)
-			return (j);
+			return (int(j));
 	}
 	return (-1);
 }
